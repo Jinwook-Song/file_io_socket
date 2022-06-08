3e828132f96d95e43d7fb2098040efe2822cde11
jinwook song <wlsdnr129@naver.com>
2022-06-08 11:37:37 +0900

    upload pdf

--- src/public/index.js
+++ src/public/index.js
@@ -5,12 +5,19 @@ const messages = document.getElementById('messages');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const videoInput = document.getElementById('videoInput');
+const imageInput = document.getElementById('imageInput');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   if (input.value) {
-    socket.emit('load video file', input.value);
+    socket.emit('load file', input.value);
     input.value = '';
+  } else {
+    const pdf = messages.childNodes[0];
+    pdf.style.width = '20%';
+    pdf.style.height = '20vh';
+    pdf.style.position = 'relative';
+    pdf.style.bottom = 'inherit';
   }
 });
 
@@ -27,7 +34,21 @@ videoInput.addEventListener('change', function (event) {
   };
 });
 
-socket.on('send video file', function (videoBuffer, videoMime = 'video/mp4') {
+imageInput.addEventListener('change', function (event) {
+  const image = event.target.files[0];
+  const imageName = image.name;
+
+  const reader = new FileReader();
+  reader.readAsArrayBuffer(image);
+  reader.onload = (event) => {
+    imageBuffer = event.target.result;
+
+    socket.emit('add image file', imageBuffer, imageName);
+  };
+});
+
+socket.on('send video file', function (videoBuffer, videoName) {
+  const videoMime = videoName.split('.')[1];
   const videoBlob = new Blob([videoBuffer], { type: videoMime });
   const videoUrl = URL.createObjectURL(videoBlob);
   const video = document.createElement('video');
@@ -38,4 +59,53 @@ socket.on('send video file', function (videoBuffer, videoMime = 'video/mp4') {
   video.src = videoUrl;
   messages.appendChild(video);
   window.scrollTo(0, document.body.scrollHeight);
+
+  video.addEventListener('dblclick', (event) => {
+    console.log('move');
+  });
+});
+
+socket.on('send image file', function (imageBuffer, imageName) {
+  const imageMime = imageName.split('.')[1];
+  console.log(imageMime);
+  const imageBlob = new Blob([imageBuffer], { type: 'application/pdf' });
+  const imageUrl = URL.createObjectURL(imageBlob);
+  const embed = document.createElement('embed');
+  embed.width = 320;
+  embed.height = 180;
+
+  embed.src = imageUrl;
+  messages.appendChild(embed);
+  window.scrollTo(0, document.body.scrollHeight);
+  embed.addEventListener('mousemove', () => {
+    console.log('moved');
+    // image.width = 640;
+    // image.height = 360;
+  });
+});
+
+messages.addEventListener('dblclick', () => {
+  console.log('clicked');
+  let pdf = messages.childNodes[0];
+
+  pdf.style.width = '100%';
+  pdf.style.height = '100vh';
+  pdf.style.position = 'absolute';
+  pdf.style.bottom = 0;
+
+  pdf = messages.childNodes[1];
+
+  pdf.style.width = '100%';
+  pdf.style.height = '100vh';
+  pdf.style.position = 'absolute';
+  pdf.style.bottom = 0;
+});
+
+const body = document.querySelector('body');
+body.addEventListener('keydown', (event) => {
+  const pdf = messages.childNodes[0];
+  pdf.style.width = '20%';
+  pdf.style.height = '20vh';
+  pdf.style.position = 'relative';
+  pdf.style.bottom = 'inherit';
 });
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import { Server } from 'socket.io';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  maxHttpBufferSize: 1e8, // 100 MB
+  maxHttpBufferSize: 1e9, // 100 MB
 });
 
 const port = 3000;
@@ -29,33 +29,52 @@ io.on('connection', (socket) => {
   });
 
   //
-  socket.on(
-    'load video file',
-    (fileName = '소방시연용_Remote_mov_201103_lite') => {
-      console.log('video file name: ' + fileName);
+  socket.on('load file', (fileName) => {
+    console.log('file name: ' + fileName);
 
-      // video
-      const videoPath = path.join(
-        __dirname,
-        '..',
-        `assets/videos/${fileName}.mp4`
-      );
-      try {
-        const videoBuffer = fs.readFileSync(videoPath);
-        console.log(videoBuffer);
-        const videoMime = mime.getType(videoPath);
-        io.emit('send video file', videoBuffer, videoMime); // emit event to everyone
-      } catch (error) {
-        console.log(error);
-      }
+    // video file
+    const videoPath = path.join(
+      __dirname,
+      '..',
+      `assets/videos/${fileName}.mp4`
+    );
+    try {
+      const videoBuffer = fs.readFileSync(videoPath);
+      console.log(videoBuffer);
+      const videoMime = mime.getType(videoPath);
+      io.emit('send video file', videoBuffer, videoMime); // emit event to everyone
+    } catch (error) {
+      console.log(error);
     }
-  );
+
+    // image file
+    const imagePath = path.join(
+      __dirname,
+      '..',
+      `assets/images/${fileName}.pdf`
+    );
+    try {
+      const imageBuffer = fs.readFileSync(imagePath);
+      console.log(imageBuffer);
+      const imageMime = mime.getType(imagePath);
+      io.emit('send image file', imageBuffer, imageMime); // emit event to everyone
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
   socket.on('add video file', (videoFile, videoName) => {
     const videoBuffer = Buffer.from(videoFile);
     const videoPath = path.join(__dirname, '..', `assets/videos/${videoName}`);
     fs.writeFileSync(videoPath, videoBuffer);
-    io.emit('send video file', videoBuffer);
+    io.emit('send video file', videoBuffer, videoName);
+  });
+
+  socket.on('add image file', (imageFile, imageName) => {
+    const imageBuffer = Buffer.from(imageFile);
+    const imagePath = path.join(__dirname, '..', `assets/images/${imageName}`);
+    fs.writeFileSync(imagePath, imageBuffer);
+    io.emit('send image file', imageBuffer, imageName);
   });
 });
 
